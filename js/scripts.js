a8e9ce03b168951bd21b4b863d52240c70507779
Colin Worf and Jenn Trass <colin@>
2017-10-17 17:01:01 -0700

    add finishing touches, enhance the click action

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-  $('.card').click(function(){
-    $('.card').not(this).removeClass('show');
-    $(this).toggleClass('show');
+  // $('.card').click(function(){
+  //   $('.card').not(this).removeClass('show');
+  //   $(this).toggleClass('show');
+  // });
+
+  $('.btn').click(function(){
+    $('.btn').not(this).parent().next().removeClass('show');
+    $(this).parent().next().toggleClass('show');
   });
 });
